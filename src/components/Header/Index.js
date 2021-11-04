4dc26b8d4afc4f84f911a6e9f400737521a2a5f5
houguang <houguang@sina.cn>
2021-11-04 14:17:35 +0800

    添加时不发送dispatch，防止重新渲染列表

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -1,15 +1,14 @@
-import {ADD_ITEM, INPUT_VALUE} from "../../store/action";
+import {ADD_ITEM} from "../../store/action";
 import {connect} from "react-redux";
 import './index.css';
 
 const Index = (props) => {
-    const {value, text, onChange, onKeyPress} = props
+    const {value, onChange, onKeyPress} = props
     return (
         <div className="todo-header">
             <input
                 type="text"
                 placeholder={value}
-                value={text || ''}
                 onChange={onChange}
                 onKeyPress={onKeyPress}
             />
@@ -26,12 +25,13 @@ const stateToProps = (state) => {
 const dispatchToProps = (dispatch) => {
     return {
         onChange: e => {
-            console.log(e.target.value)
-            dispatch({type: INPUT_VALUE, payload: {value: e.target.value}})
+            //发送dispatch时会导致重新渲染
+            //dispatch({type: INPUT_VALUE, payload: {value: e.target.value}})
         },
         onKeyPress:e=>{
             if (e.charCode === 13 && e.target.value){
-                dispatch({type:ADD_ITEM})
+                dispatch({type:ADD_ITEM,payload:{text:e.target.value}})
+                e.target.value = ''
             }
         }
     }
