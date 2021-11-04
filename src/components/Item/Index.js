abb85cf9d2f02746383f21b983f87cb7c3dab614
houguang <houguang@sina.cn>
2021-11-04 14:16:29 +0800

    鼠标移入移出显示隐藏删除按钮

--- src/components/Item/Index.js
+++ src/components/Item/Index.js
@@ -3,55 +3,53 @@ import {connect} from "react-redux";
 import {DELETE_ITEM, SET_SELECT_ITEM} from "../../store/action";
 import {useState} from "react";
 
-function Index(props){
+const Index = (props) => {
 
-    const [showDeleteButton, setShowDeleteButton] = useState(false);
-
+    const [mouse, setMouse] = useState(false);
     return (
         <li
-            onMouseEnter={()=>setShowDeleteButton(true)}
-            onMouseLeave={()=>setShowDeleteButton(false)}
+            onMouseEnter={() => setMouse(true)}
+            onMouseLeave={() => setMouse(false)}
+            style={{backgroundColor: mouse ? '#ccc' : 'white'}}
         >
             <label>
                 <input
                     type="checkbox"
                     checked={props.todo.select}
-                    onClick={()=>props.setSelect(props.index)}
-                    onChange={props.selectChange}
+                    onChange={()=>{}}
+                    onClick={() => props.setSelect(props.index)}
                 />
                 <span>{props.todo.text}</span>
             </label>
             <button
                 className="btn btn-danger"
-                style={{display:showDeleteButton?'block':'none'}}
-                onClick={()=>props.deleteItem(props.index)}
-            >删除</button>
+                style={{display: mouse ? 'block' : 'none'}}
+                onClick={() => props.deleteItem(props.index)}
+            >删除
+            </button>
         </li>
     )
 }
 
 const stateToProps = (state) => {
     return {
-        showDeleteButton:1,
+        showDeleteButton: 1,
         ...state
     }
 }
 
-const dispatchToProps = dispatch=>{
+const dispatchToProps = dispatch => {
     return {
-        setSelect(index){
+        setSelect(index) {
             console.log(index)
-            dispatch({type:SET_SELECT_ITEM,payload:{index}})
+            dispatch({type: SET_SELECT_ITEM, payload: {index}})
         },
-        selectChange(){},
-        mouseOver(){},
-        mouseOut(){},
-        deleteItem(index){
-            if (window.confirm("确定要删除吗?")){
-                dispatch({type:DELETE_ITEM,payload:{index}})
+        deleteItem(index) {
+            if (window.confirm("确定要删除吗?")) {
+                dispatch({type: DELETE_ITEM, payload: {index}})
             }
         }
     }
 }
 
-export default connect(stateToProps,dispatchToProps)(Index);
+export default connect(stateToProps, dispatchToProps)(Index);
